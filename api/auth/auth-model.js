146b68a50d3bb1045745640f0b7eff8659f0922d
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-23 11:13:32 -0600

    add model function returns new user

--- api/auth/auth-model.js
+++ api/auth/auth-model.js
@@ -1,9 +1,22 @@
 const db = require('../../data/dbConfig')
 
+async function findById(id) {
+    const [user] = await db('users').where('id', id)
+    return user
+}
+
 async function add(user) {
-    console.log('add wired')
+    const [id] = await db('users').insert(user)
+    return findById(id)
+}
+
+async function findByUsername(username) {
+    const user = await db('users').where('username', username)
+    return user
 }
 
 module.exports = {
-    add
+    add,
+    findById,
+    findByUsername
 }
