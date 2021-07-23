18664e23bbf0e538777fcdbcd8f06e971567ac96
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-23 11:51:38 -0600

    creates restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,26 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../config/secrets')
+const Users = require('./../auth/auth-model')
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if(!token) {
+    return next({
+      status: 401,
+      message: 'token required'
+    })
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if(err){
+      return next({
+        status: 401,
+        message: 'token invalid'
+      })
+    }
+    req.decodedJwt = decodedToken
+    next()
+  })
+
   /*
     IMPLEMENT
 
