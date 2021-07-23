8d43d6b3193168b2443150e54fabce3973e5abe0
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-23 12:38:49 -0600

    creates POST /login tests

--- api/server.test.js
+++ api/server.test.js
@@ -29,11 +29,48 @@ describe('[POST] /register', () => {
     })
     expect(await db('users')).toHaveLength(4)
   })
-  test.todo('responds with id, username, password on successful register')
-  test.todo('responds with [username and password required] if username missing')
+  test('responds with id, username, password on successful register', async () => {
+    const res = await request(server).post('/api/auth/register').send({
+      username: 'sandy',
+      password: '1234'
+    })
+    expect(res.body).toMatchObject({ id: 4, username: 'sandy' })
+  })
+  test('responds with [username and password required] if password missing', async () => {
+    const res = await request(server).post('/api/auth/register').send({
+      username: 'sandy'
+    })
+    expect(res.body.message).toBe('username and password required')
+  })
 })
 
 describe('[POST] /login', () => {
-  test.todo('responds with {message: welcome, [username], token} on successful login')
-  test.todo('responds with invalid credentials if incorrect username')
+  test('responds with {message: welcome, [username], token} on successful login', async () => {
+    await request(server).post('/api/auth/register').send({
+      username: 'sandy',
+      password: '1234'
+    }) 
+    const res = await request(server).post('/api/auth/login').send({
+      username: 'sandy',
+      password: '1234'
+    })
+    expect(res.body.message).toBe('welcome, sandy')
+  })
+  test('responds with [invalid credentials] message if incorrect username', async () => {
+    await request(server).post('/api/auth/register').send({
+      username: 'sandy',
+      password: '1234'
+    }) 
+    const res = await request(server).post('/api/auth/login').send({
+      username: 'sand',
+      password: '1234'
+    })
+    expect(res.body.message).toBe('invalid credentials')
+  })
+  test('responds with [username and password required] message if password is missing', async () => {
+    const res = await request(server).post('/api/auth/login').send({
+      username: 'sandy'
+    })
+    expect(res.body.message).toBe('username and password required')
+  })
 })
