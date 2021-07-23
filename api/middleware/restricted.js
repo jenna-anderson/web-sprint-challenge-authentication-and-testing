15dd34f694b5841b8a40e50ea1aa6326f79d6920
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-23 12:40:34 -0600

    refactors

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../../config/secrets')
-const Users = require('./../auth/auth-model')
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
@@ -20,16 +19,4 @@ module.exports = (req, res, next) => {
     req.decodedJwt = decodedToken
     next()
   })
-
-  /*
-    IMPLEMENT
-
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
 };
